640df84e5ff9fcf7ad9d6b77da398d027eeb53c6
eeswk <inter-lsw@hanmail.net>
2021-04-08 01:19:25 +0900

    example api test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import WeatherApp from './WeatherApp';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+    const [keyword, setKeyword] = useState([]);
+
+
+    const fetchDate = () => {
+        //REST API CALL comes here
+        const url = `https://api.github.com/search/repositories?q=${keyword}`;
+        fetch(url)
+        .then(response => response.json())
+        .then(responseData => {
+            setData(responseData.items);
+        });
+    }    
+
+    const handleChange = (e) => {
+        setKeyword(e.target.value);
+    }
+
+    const tableRows = data.map((item, index) => 
+        <tr key={index}>
+            <td>{item.full_name}</td>
+            <td><a href={item.html_url}>{item.html_url}</a></td>    
+        </tr>
+    );
+
+
+    return (
+        <div className="App">
+            <WeatherApp />
+            <input type="text" onChange={handleChange} />
+            <button onClick={fetchDate} value={keyword} >fetch</button>
+            <table><tbody>{tableRows}</tbody></table>
+        </div>
+    );
 }
 
 export default App;
