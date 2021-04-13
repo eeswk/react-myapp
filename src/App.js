77361f66f958946fde0ae882fd1beb17e543e979
eeswk <inter-lsw@hanmail.net>
2021-04-14 00:42:59 +0900

    router, props

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import { useState } from 'react';
 import ReactTable from "react-table-6";
 import "react-table-6/react-table.css" ;
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import AddItem from './AddItem';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import Contact from './Contract';
+import Home from './Home';
 
 function App() {
     const [data, setData] = useState([]);
     const [keyword, setKeyword] = useState('');
+
+    const [items, setItems] = useState([]);
 
 
     const fetchDate = () => {
@@ -55,14 +69,46 @@ function App() {
         alert(value);
     }
 
+    const addItem = (item) => {
+        setItems([item, ...items]);
+    }
+
+    const listItems = items.map((item, index) => 
+    <ListItem key={index}>
+    <ListItemText primary={item.product} secondary={item.amount} />
+    </ListItem>);
+
 
     return (
         <div className="App">
+            <BrowserRouter>
+                <div>
+                    <Link to="/">Home</Link>{' '}
+                    <Link to="/contact">Contact</Link>{' '} 
+                    <Link to="/links">Links</Link>{' '} 
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/contact" component={Contact} />
+                        <Route path="/links" render={() => <h1>Links</h1>} />
+                        <Route render={() => <h1>Page not found</h1>} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+
             <WeatherApp />
             <input type="text" onChange={handleChange} />
             <button onClick={fetchDate} value={keyword} >fetch</button>
             <ReactTable data={data} columns={columns} filterable={true} defaultPageSize={10} />
-           
+
+            <AppBar position="static" color="default">
+                <Toolbar>
+                    <Typography variant="h6" color="inherit">
+                        SHOPPINGLIST
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <AddItem addItem1={addItem}></AddItem>
+            <List>{listItems}</List>
         </div>
     );
 }
